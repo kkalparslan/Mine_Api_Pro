4780a05e8b7f1183d663a7fc8b04bb94bab45789
alparslan <alparslanztk@gmail.com>
2023-01-29 14:04:51 +0300

    new post request added 29.01.23

--- src/test/java/apiTest/day7_POST_Request/PostEducation.java
+++ src/test/java/apiTest/day7_POST_Request/PostEducation.java
@@ -4,10 +4,11 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
+import java.util.HashMap;
+import java.util.Map;
 import static io.restassured.RestAssured.*;
 import static org.testng.Assert.*;
-
+import static org.hamcrest.Matchers.*;
 
 public class PostEducation {
 
@@ -57,8 +58,73 @@ public class PostEducation {
         String name="pink9";
         String email="pink9@krafttechexlab.com";
         String password="pink9";
-        String about="about me";
+        String about="structor";
         String terms="4";
 
+        Map<String,Object>requestMap=new HashMap<>();
+        requestMap.put("name", name);
+        requestMap.put("email", email);
+        requestMap.put("password", password);
+        requestMap.put("about", about);
+        requestMap.put("terms", terms);
+
+        Response response = given().accept(ContentType.JSON)
+                .and()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(requestMap)
+                .when()
+                .post("/allusers/register");
+        assertEquals(response.statusCode(), 200);
+        String token=response.path("token");
+        //String userID=response.path("id");
+
+        Map<String,Object>educationBody=new HashMap<>();
+        educationBody.put("school", "Krafttech");
+        educationBody.put("degree", "Bootcamp");
+        educationBody.put("study", "QA");
+        educationBody.put("fromdate", "2022-02-02");
+        educationBody.put("todate", "2023-02-02");
+        educationBody.put("current", "false");
+        educationBody.put("description", "structor");
+
+        response=given().accept(ContentType.JSON)
+                .and()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(educationBody)
+                .header("token", token)
+                .when().post("/education/add");
+        response.prettyPrint();
+        assertEquals(response.statusCode(), 200);
+
+        // verify body
+        int id=response.path("id");
+        response=given().accept(ContentType.JSON)
+                .and()
+                .header("token", token) // swaggerda querry yada headre olarak değişebiliyor. dikkat edilmeli
+                .pathParam("id", id)
+                .when()
+                .get("/education/getbyid/{id}");
+        response.prettyPrint();
+        assertEquals(response.statusCode(), 200);
+
+        //verify with path
+        //System.out.println("userID = " + userID);
+        System.out.println("id = " + id);
+        assertEquals(response.path("school"), "Krafttech");
+        //assertEquals(response.path("userid"), userID);
+
+        //verify using hamcrest matcher
+        given().contentType(ContentType.JSON)
+                .and()
+                .header("token", token) //swaggerda querry veya header olarak değişebiliyor. dikkatli olmak gerek
+                .pathParam("id", id)
+                .when()
+                .get("/education/getbyid/{id}")
+                .then()
+                .assertThat()
+                .body("school", equalTo("Krafttech"), "study", equalTo("QA"))
+                .log().all();
     }
 }
