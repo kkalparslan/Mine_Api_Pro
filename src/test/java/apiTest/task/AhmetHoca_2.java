6c69b6ddf382f21a0dfeddf5b478c3a734ff8857
alparslan <alparslanztk@gmail.com>
2023-01-30 17:39:38 +0300

    task codes added 30.01.23

---
+++ src/test/java/apiTest/task/AhmetHoca_2.java
@@ -0,0 +1,16 @@
+package apiTest.task;
+
+public class AhmetHoca_2 {
+
+    /**
+     * Asagidaki API dokumanini inceleyiniz. Postmande yeni bir collection olusturun ve asagidaki gorevleri tamamlayiniz.
+     * Go to https://demoqa.com/swagger/Links to an external site.
+     * Create new user
+     * Generate Token
+     * Authorized
+     * Add 3 New Books
+     * Get User Info
+     * Update 1 book
+     * Delete 1 book
+     */
+}
